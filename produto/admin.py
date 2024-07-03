e43c7259d08eb35340114040a3375f9479369b97
cara <cara@workspace.myguest.virtualbox.org>
2024-07-03 14:21:28 -0300

    variações de produtos

--- produto/admin.py
+++ produto/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import Produto
+from .models import Produto, Variacao
 
-admin.site.register(Produto)
+class VariacaoInline(admin.TabularInline):
+    model = Variacao
+    extra = 1
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    inlines = [
+        VariacaoInline
+    ]
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Variacao)
